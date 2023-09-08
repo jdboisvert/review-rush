238ad740bd0f05bc5e3d805e3eeffbcda4209fff
jdboisvert <info.jeffreyboisvert@gmail.com>
2023-09-07 22:11:29 -0400

    feat: GitHub happy paths covered by tests and include dependency injection of the client to help with testing.

--- pkg/github/service.go
+++ pkg/github/service.go
@@ -1,6 +1,14 @@
 package github
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const apiURL = "https://api.github.com"
 
 type GithubService interface {
 	GetRepos(ctx context.Context) ([]Repository, error)
@@ -9,25 +17,98 @@ type GithubService interface {
 }
 
 type service struct {
-	token string
-	org   string
+	client *http.Client
+	token  string
+	org    string
 }
 
 func NewGitHubService(token, org string) GithubService {
 	return &service{
-		token: token,
-		org:   org,
+		token:  token,
+		org:    org,
+		client: &http.Client{},
 	}
 }
 
 func (s *service) GetRepos(ctx context.Context) ([]Repository, error) {
-	return []Repository{}, nil
+	url := fmt.Sprintf("%s/orgs/%s/repos", apiURL, s.org)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+s.token)
+
+	resp, err := s.client.Do(req.WithContext(ctx))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var repos []Repository
+	if err := json.Unmarshal(body, &repos); err != nil {
+		return nil, err
+	}
+
+	return repos, nil
 }
 
 func (s *service) GetPullRequests(ctx context.Context, repo string) ([]PullRequest, error) {
-	return []PullRequest{}, nil
+	url := fmt.Sprintf("%s/repos/%s/%s/pulls", apiURL, s.org, repo)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+s.token)
+
+	resp, err := s.client.Do(req.WithContext(ctx))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var prs []PullRequest
+	if err := json.Unmarshal(body, &prs); err != nil {
+		return nil, err
+	}
+
+	return prs, nil
 }
 
 func (s *service) GetReviews(ctx context.Context, prURL string) ([]Review, error) {
-	return []Review{}, nil
+	req, err := http.NewRequest(http.MethodGet, prURL+"/reviews", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+s.token)
+
+	resp, err := s.client.Do(req.WithContext(ctx))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var reviews []Review
+	if err := json.Unmarshal(body, &reviews); err != nil {
+		return nil, err
+	}
+
+	return reviews, nil
 }
--- pkg/github/service_test.go
+++ pkg/github/service_test.go
@@ -1,49 +1,124 @@
 package github
 
 import (
+	"bytes"
 	"context"
+	"io"
+	"net/http"
 	"reflect"
 	"testing"
 )
 
-func TestGetRepos(t *testing.T) {
-	s := NewGitHubService("dummyToken", "dummyOrg")
+type MockTransport struct {
+	Resp *http.Response
+	Err  error
+}
 
-	repos, err := s.GetRepos(context.Background())
-	if err != nil {
-		t.Fatalf("expected no error, but got: %v", err)
+func (m *MockTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	return m.Resp, m.Err
+}
+
+func TestNewGitHubService(t *testing.T) {
+	token := "testToken"
+	org := "testOrg"
+
+	serviceInstance := NewGitHubService(token, org)
+
+	s, ok := serviceInstance.(*service)
+	if !ok {
+		t.Fatalf("Expected type *service; got %T", serviceInstance)
 	}
 
-	expectedRepos := []Repository{}
-	if !reflect.DeepEqual(repos, expectedRepos) {
-		t.Fatalf("expected %v, but got %v", expectedRepos, repos)
+	if s.token != token {
+		t.Errorf("Expected token to be %s; got %s", token, s.token)
+	}
+
+	if s.org != org {
+		t.Errorf("Expected org to be %s; got %s", org, s.org)
+	}
+
+	if reflect.TypeOf(s.client).String() != "*http.Client" {
+		t.Errorf("Expected client to be of type *http.Client; got %T", s.client)
+	}
+}
+
+func TestGetRepos(t *testing.T) {
+	responseBody := `[{"id": 1, "name": "repo1"}, {"id": 2, "name": "repo2"}]`
+	mockResp := &http.Response{
+		StatusCode: 200,
+		Body:       io.NopCloser(bytes.NewReader([]byte(responseBody))),
+	}
+
+	client := &http.Client{
+		Transport: &MockTransport{Resp: mockResp},
+	}
+
+	service := &service{
+		client: client,
+		token:  "sample_token",
+		org:    "sample_org",
+	}
+
+	repos, err := service.GetRepos(context.Background())
+	if err != nil {
+		t.Fatalf("expected no error but got: %v", err)
+	}
+
+	if len(repos) != 2 {
+		t.Fatalf("expected 2 repos but got %d", len(repos))
 	}
 }
 
 func TestGetPullRequests(t *testing.T) {
-	s := NewGitHubService("dummyToken", "dummyOrg")
-
-	prs, err := s.GetPullRequests(context.Background(), "testRepo")
-	if err != nil {
-		t.Fatalf("expected no error, but got: %v", err)
+	responseBody := `[{"id": 1, "title": "PR1"}, {"id": 2, "title": "PR2"}]`
+	mockResp := &http.Response{
+		StatusCode: 200,
+		Body:       io.NopCloser(bytes.NewReader([]byte(responseBody))),
 	}
 
-	expectedPRs := []PullRequest{}
-	if !reflect.DeepEqual(prs, expectedPRs) {
-		t.Fatalf("expected %v, but got %v", expectedPRs, prs)
+	client := &http.Client{
+		Transport: &MockTransport{Resp: mockResp},
+	}
+
+	service := &service{
+		client: client,
+		token:  "sample_token",
+		org:    "sample_org",
+	}
+
+	prs, err := service.GetPullRequests(context.Background(), "sample_repo")
+	if err != nil {
+		t.Fatalf("expected no error but got: %v", err)
+	}
+
+	if len(prs) != 2 {
+		t.Fatalf("expected 2 pull requests but got %d", len(prs))
 	}
 }
 
 func TestGetReviews(t *testing.T) {
-	s := NewGitHubService("dummyToken", "dummyOrg")
-
-	reviews, err := s.GetReviews(context.Background(), "https://test.com/pr/1")
-	if err != nil {
-		t.Fatalf("expected no error, but got: %v", err)
+	responseBody := `[{"id": 1, "user": {"login": "user1"}}, {"id": 2, "user": {"login": "user2"}}]`
+	mockResp := &http.Response{
+		StatusCode: 200,
+		Body:       io.NopCloser(bytes.NewReader([]byte(responseBody))),
 	}
 
-	expectedReviews := []Review{}
-	if !reflect.DeepEqual(reviews, expectedReviews) {
-		t.Fatalf("expected %v, but got %v", expectedReviews, reviews)
+	client := &http.Client{
+		Transport: &MockTransport{Resp: mockResp},
+	}
+
+	service := &service{
+		client: client,
+		token:  "sample_token",
+		org:    "sample_org",
+	}
+
+	reviews, err := service.GetReviews(context.Background(), "https://api.github.com/repos/sample_org/sample_repo/pulls/1")
+	if err != nil {
+		t.Fatalf("expected no error but got: %v", err)
+	}
+
+	if len(reviews) != 2 {
+		t.Fatalf("expected 2 reviews but got %d", len(reviews))
 	}
 }
